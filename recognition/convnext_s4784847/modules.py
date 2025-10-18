c419eb02e1b9017d5d71fbb428a6770a7c64c7b1
Wasana Udabage <wasanaudabge16@gmail.com>
2025-10-18 21:25:18 +1000

    feat(modules): Add simple ConvNeXBlock implementation

--- recognition/convnext_s4784847/modules.py
+++ recognition/convnext_s4784847/modules.py
@@ -14,6 +14,33 @@ class ConvNeXtBlock(nn.Module):
     def __init__(self, dimentions):
         super().__init__()
 
+        self.dwconv = nn.Conv2d(dimentions, dimentions, kernel_size=7, padding=3, groups=dimentions)
+
+        self.norm = nn.LayerNorm(dimentions,eps=1e-6)
+
+        self.pwconv1 = nn.Linear(dimentions,  4*dimentions)
+        self.act = nn.GELU
+        self.pwconv2 = nn.Linear(4* dimentions, dimentions)
+
+    def forward(self,x):
+
+        residual = x
+
+        x = self.dwconv(x)
+
+        x = x.permute(0,2,3,1)
+        x = self.norm(x)
+
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+
+        x = x.permute(0,3,1,2)
+
+        x = residual + x
+
+        return x
+
 
 class DownsampleLayer():
 
